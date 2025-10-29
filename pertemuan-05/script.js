fe1f8c35617aa693dbc7adc6946b05469062bdd6
IRSYA EVA SAFITRI <2511500085@mahasiswa.atmaluhur.ac.id>
2025-10-29 21:55:51 +0700

    memperbaiki script.js (menu toggle)

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -79,3 +79,15 @@ window.addEventListener("resize", () => {
         if (target) alignErrorMessage(small, target);
     });
 });
+
+
+document.getElementById("menuToggle").addEventListener("click", function () {
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("active");
+    
+    if (nav.classList.contains("active")) {
+        this.textContent = "\u2716";
+    } else {
+        this.textContent = "\u2630";
+    }
+});
