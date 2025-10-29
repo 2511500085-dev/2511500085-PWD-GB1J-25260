7de7e4a1829f23d669277f7c24cc1f85240c52a9
IRSYA EVA SAFITRI <2511500085@mahasiswa.atmaluhur.ac.id>
2025-10-29 22:11:34 +0700

    memperbaiki script.js (ucapan)

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -94,3 +94,11 @@ document.getElementById("menuToggle").addEventListener("click", function () {
         this.textContent = "\u2630";
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const homeSection = document.getElementById("home");
+    const ucapan = document.createElement("p");
+    ucapan.textContent = "Halo! Selamat datang di halaman saya!";
+    homeSection.appendChild(ucapan);
+});
