27c1ef2b974a1f75d0420fcca0a5138a854a2392
unknown <2511500083@mahasiswa.atmaluhur.ac.id>
2025-10-30 23:16:10 +0700

    memperbaiki latihankonstanta.php

--- pertemuan-06/latihankonstanta.php
+++ pertemuan-06/latihankonstanta.php
@@ -1,7 +1,9 @@
 <?php
 define("DOSEN_PENGAMPU", "bpk.Yohanes Setiawan");
+define("MATA_KULIAH", "Pemrograman Web Dasar");
 const ANGKATAN = 2025;
 
 echo "DOSEN_PENGAMPU: " . DOSEN_PENGAMPU . "<br>";
-echo "ANGKATAN: " . ANGKATAN . ;
+echo "ANGKATAN: " . ANGKATAN . "<br>";
+echo "Mata Kuliah: " . MATA_KULIAH;
 ?>
