4066dc837b332daaf687b3341155f38056da50fb
unknown <2511500083@mahasiswa.atmaluhur.ac.id>
2025-10-30 22:55:15 +0700

    memperbaiki latihankonstanta.php

--- pertemuan-06/latihankonstanta.php
+++ pertemuan-06/latihankonstanta.php
@@ -1,9 +1,9 @@
 <?php
-define("KAMPUS", "ISB Atma Luhur" "Dosen_pengampu", "Mata kuliah");
+define("KAMPUS", "ISB Atma Luhur", "Dosen_pengampu", "Mata kuliah");
 const ANGKATAN = 2025;
 echo "Kampus: " . KAMPUS . "<br>";
 echo "Angkatan: " . ANGKATAN . "<br>";
-echo "Dosen: " . Dosen_pengampu . "<br>";
-echo "Mata kuliah: " . Mata kuliah . "<br>"; 
-
+echo "Dosen_pengampu: " . BAPAK YOHANES . "<br>";
+echo "Mata kuliah: " . PEMROGRAMAN WEB DASAR . "<br>"; 
+ 
 ?>
