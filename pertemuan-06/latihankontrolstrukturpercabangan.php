59dbe3d5556121bdb2beb589376819c95756ce89
unknown <2511500083@mahasiswa.atmaluhur.ac.id>
2025-11-05 23:13:17 +0700

    menambahkan latihankontrolstrukturpercabangan.php

--- pertemuan-06/latihankontrolstrukturpercabangan.php
+++ pertemuan-06/latihankontrolstrukturpercabangan.php
@@ -8,3 +8,12 @@ echo "B";
 echo "C";
 }
 ?>
+
+<?php
+$hari = "Senin";
+switch ($hari) {
+case "Senin": echo "Awal Minggu!"; break;
+case "Jumat": echo "Hampir weekend!"; break;
+default: echo "Hari biasa.";
+}
+?>
