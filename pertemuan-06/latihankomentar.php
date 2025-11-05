d48f3c5ca79ab5099dd02bdfc5e060d2db9657e4
unknown <2511500083@mahasiswa.atmaluhur.ac.id>
2025-11-06 00:00:23 +0700

    memperbaiki latihankomentar.php

--- pertemuan-06/latihankomentar.php
+++ pertemuan-06/latihankomentar.php
@@ -5,4 +5,12 @@
     Ini komentar
     lebih dari satu baris
 */
+
+// Tapi ini akan tampil di browser ↓
+echo "Ini contoh komentar yang ditampilkan di browser<br>";
+echo "Ini komentar 1 baris<br>";
+echo "Ini juga komentar 1 baris<br>";
+echo "Ini komentar lebih dari satu baris:<br>";
+echo "Baris pertama<br>";
+echo "Baris kedua<br>";
 ?>
