a472745f645fe5024a0d357bf52672d11d934d60
IRSYA EVA SAFITRI <2511500085@mahasiswa.atmaluhur.ac.id>
2025-11-10 23:10:02 +0700

    memperbaiki proses.php

--- pertemuan-07/get_proses.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION["nama"] = $POST["txtNama"];
-$_SESSION["email"] = $POST["txtEmail"];
-$_SESSION["pesan"] = $POST["txtPesan"];
-echo $_SESSION["nama"] . " " .  $_SESSION["email"] . " " .  $_SESSION["pesan"];
-header("Location: proses.php");
-?>
