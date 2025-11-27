b68b7b5c4f77facaa4af1d4ee3e626f75d5b9536
IRSYA EVA SAFITRI <2511500085@mahasiswa.atmaluhur.ac.id>
2025-11-27 18:28:50 +0700

    menambahkan fungsi.php

--- pertemuan-09/index.php
+++ pertemuan-09/index.php
@@ -118,22 +118,13 @@ $fieldConfig = [
 
     <section id="about">
       <h2>Tentang Saya</h2>
-      <?php foreach ($fieldconfig as $kunci => $metadata): ?>
+      <?php foreach ($fieldConfig as $kunci => $metadata): ?>
         <p> 
           <strong><?= $metadata["label"] ?></strong>
           <?= htmlspecialchars($biodata[$kunci] ?? "") ?>
           <?= $metadata["suffix"]?>
-          
-      <p><strong>NIM:</strong> <?= $txtNim ?></p>
-      <p><strong>Nama Lengkap:</strong> <?= $txtNmLengkap ?> &#128526;</p>
-      <p><strong>Tempat Lahir:</strong> <?= $txtT4Lhr ?></p>
-      <p><strong>Tanggal Lahir:</strong> <?= $txtTglLhr ?></p>
-      <p><strong>Hobi:</strong> <?= $txtHobi ?> &#127926;</p>
-      <p><strong>Pasangan:</strong> <?= $txtPasangan ?> &hearts;</p>
-      <p><strong>Pekerjaan:</strong> <?= $txtKerja ?> &copy; 2025</p>
-      <p><strong>Nama Orang Tua:</strong> <?= $txtNmOrtu ?></p>
-      <p><strong>Nama Kakak:</strong> <?= $txtNmKakak ?></p>
-      <p><strong>Nama Adik:</strong> <?= $txtNmAdik ?></p>
+        </p> 
+      <?php endforeach; ?>
     </section>
 
     <section id="contact">
