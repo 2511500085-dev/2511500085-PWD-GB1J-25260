da090ed350d2f42b49e363cace29cc156dfdca2a
IRSYA EVA SAFITRI <2511500085@mahasiswa.atmaluhur.ac.id>
2026-01-07 11:44:37 +0700

    menambahkan  kode di edit.php

--- pertemuan-12/edit.php
+++ pertemuan-12/edit.php
@@ -42,3 +42,61 @@
         $cpesan = $old['cpesan'] ?? $cpesan;
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>judul halaman</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>ini header</h1>
+            <button class="menu-toggle" id="menuToggle" aria-label="Toggle Navigation" >
+                &#9776;
+            </button>
+            <nav>
+                <ul>
+                    <li><a href="#home">Beranda</a></li>
+                    <li><a href="#about">Tentang</a></li>
+                    <li><a href="#contact">Kontak</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <section id="contact">
+                <h2>Edit Data Tamu</h2>
+                <?php if (!empty($flash_error)): ?>
+                    <div style="padding:10px; margin-bottom:10px;
+                    background:#f8d7da; color:#721c24; border-radius:6px;">
+                        <?=$flash_error;?>
+                    </div>
+                <?php endif; ?>
+                <form action="proses_update.php" method="POST">
+                    <input type="text" name="cid" value="<?= (int)$cid; ?>">
+        
+                        <label for="txtNama"><span>Nama:</span>
+                        <input type="text" id="txtNama" name="txtNamaEd"
+                        placeholder="masukkan nama"  required autocomplete="name"
+                         value="<?= !empty($nama) ? $nama :'' ?> ">
+                        </label>
+
+                        <label for="txtEmail"><span>Email:</span>
+                        <input type="Email" id="txtEmail" name="txtEmailEd"
+                        placeholder="masukkan email" required autocomplete="email"
+                         value="<?= !empty($email) ? $email :'' ?>">
+                        </label>
+
+                        <label for="txtPesan"><span>Pesan:</span>
+                        <textarea id="txtPesan" name="txtPesanEd" rows="4"
+                        placeholder= "tulis pesan anda..."
+                         required><?= !empty($pesan) ? $pesan : '' ?></textarea>
+                        </label>
+
+                    <div>
+                        <button type="submit">Update</button>
+                    </div>
+                </form>
+            
