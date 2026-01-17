a0d91c47cc9d2f7ece8228c6d8cf0a0663d53ed4
irsya eva safitri <newirsya@gmail.com>
2026-01-17 12:05:34 +0700

    menyimpan pertemuan 15

--- pertemuan-15/readmahasiswa.php
+++ pertemuan-15/readmahasiswa.php
@@ -46,14 +46,14 @@
     <tr>
       <td><?= $i++ ?></td>
       <td>
-        <a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a>
-         <a onclick="return confirm('hapus <?= htmlspecialchars($row['cnama']); ?>?')" href="proses_delete.php?cid=<?= (int)$row['cid']; ?>">Delete</a>
+        <a href="editmahasiswa.php?cid=<?= (int)$row['cid']; ?>">Edit</a>
+         <a onclick="return confirm('hapus <?= htmlspecialchars($row['cnama']); ?>?')" href="proses_deletemahasiswa.php?cid=<?= (int)$row['cid']; ?>">Delete</a>
       </td>
       <td><?= $row['cid']; ?></td>
       <td><?= htmlspecialchars($row['cnama']); ?></td>
       <td><?= htmlspecialchars($row['cemail']); ?></td>
       <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
-      <td><?= $row['dcreated_at']; ?></td> 
+      <td><?= formatTanggal(htmlspecialchars($row['created_at'])); ?></td> 
     </tr>
   <?php endwhile; ?>
-</table>
+</table>s
