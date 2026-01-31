322d6e1929f8152a54b2751ff2d00dd30f60440e
irsya eva safitri <newirsya@gmail.com>
2026-01-31 11:09:17 +0700

    mengganti kodekode yang bersangkutan di proses_anggota.php

--- pertemuan-16/proses_anggota.php
+++ pertemuan-16/proses_anggota.php
@@ -11,43 +11,65 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 
 #ambil dan bersihkan nilai dari form
-$nama  = bersihkan($_POST['txtNama']  ?? '');
-$email = bersihkan($_POST['txtEmail'] ?? '');
-$pesan = bersihkan($_POST['txtPesan'] ?? '');
-$captcha = bersihkan($_POST['txtCaptcha'] ?? '');
+$Nomor  = bersihkan($_POST['txtNoAng']  ?? '');
+$Nama = bersihkan($_POST['txtNmAng'] ?? '');
+$Jabatan = bersihkan($_POST['txtJabAng'] ?? '');
+$Tanggaljadi = bersihkan($_POST['txtTglJadi'] ?? '');
+$Kemampuan = bersihkan($_POST['txtSkill'] ?? '');
+$Gaji  = bersihkan($_POST['txtGaji']  ?? '');
+$NomorWA  = bersihkan($_POST['txtNoWA']  ?? '');
+$Batalion  = bersihkan($_POST['txBatalion']  ?? '');
+$Beratbadan = bersihkan($_POST['txtBB']  ?? '');
+$Tinggibadan = bersihkan($_POST['txtTB']  ?? '');
 
 #Validasi sederhana
 $errors = []; #ini array untuk menampung semua error yang ada
 
-if ($nama === '') {
+
+if ($Nomor === '') {
+  $errors[] = 'Nomor wajib diisi.';
+}
+
+if ($Nama === '') {
   $errors[] = 'Nama wajib diisi.';
 }
 
-if ($email === '') {
-  $errors[] = 'Email wajib diisi.';
-} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  $errors[] = 'Format e-mail tidak valid.';
+if ($Tanggaljadi === '') {
+  $errors[] = 'Tanggal wajib diisi.';
+
+if ($Jabatan === '') {
+  $errors[] = 'Jabatan wajib diisi.';
 }
 
-if ($pesan === '') {
-  $errors[] = 'Pesan wajib diisi.';
+if ($Kemampuan === '') {
+  $errors[] = 'skill wajib diisi.';
 }
 
-if ($captcha === '') {
-  $errors[] = 'Pertanyaan wajib diisi.';
+if ($Gaji === '') {
+  $errors[] = 'gaji wajib diisi.';
+}
+if ($NomorWA === '') {
+  $errors[] = 'NoWA wajib diisi.';
+}
+if ($Batalion === '') {
+  $errors[] = 'batalion wajib diisi.';
+}
+if ($Beratbadan === '') {
+  $errors[] = 'BB wajib diisi.';
 }
 
-if (mb_strlen($nama) < 3) {
-  $errors[] = 'Nama minimal 3 karakter.';
+if ($Tinggibadan === '') {
+  $errors[] = 'TB wajib diisi.';
 }
 
-if (mb_strlen($pesan) < 10) {
-  $errors[] = 'Pesan minimal 10 karakter.';
+if (mb_strlen($Nama) < 3) {
+  $errors[] = 'Nama minimal 5 karakter.';
 }
 
-if ($captcha!=="5") {
-  $errors[] = 'Jawaban '. $captcha.' captcha salah.';
+if (mb_strlen($Tanggaljadi) < 10) {
+  $errors[] = 'tanggal minimal 8 karakter.';
 }
+
 
 /*
 kondisi di bawah ini hanya dikerjakan jika ada error, 
@@ -58,6 +80,7 @@ if (!empty($errors)) {
     'Nomor'  => $Nomor,
     'Nama' => $Nama,
     'Jabatan' => $Jabatan,
+    'Tanggaljadi' => $Tanggaljadi,
     'Kemampuan' => $Kemampuan,
     'Gaji' => $Gaji,
     'NomorWA' => $NomorWA,
@@ -71,17 +94,17 @@ if (!empty($errors)) {
 }
 
 #menyiapkan query INSERT dengan prepared statement
-$sql = "INSERT INTO anggota (Nomor, Nama, Jabatan, Kemampuan, Gaji, NommorWA, Batalion, Beratbadan, Tinggibadan) 
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$sql = "INSERT INTO anggota (Nomor, Nama, Jabatan, Tanggaljadi, Kemampuan, Gaji, NommorWA, Batalion, Beratbadan, Tinggibadan) 
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 $stmt = mysqli_prepare($conn, $sql);
 
 if (!$stmt) {
   #jika gagal prepare, kirim pesan error ke pengguna (tanpa detail sensitif)
   $_SESSION['flash_error'] = 'Terjadi kesalahan sistem (prepare gagal).';
-  redirect_ke('index.php#contact');
+  redirect_ke('indexanggota.php#anggota');
 }
 #bind parameter dan eksekusi (s = string)
-mysqli_stmt_bind_param($stmt, "sssssssss", $Nomor, $Nama, $Jabatan, $Kemampuan, $Gaji, $NomorWA, $Batalion, $Beratbadan, $Tinggibadan);
+mysqli_stmt_bind_param($stmt, "ssssssssss", $Nomor, $Nama, $Jabatan, $Kemampuan, $Gaji, $NomorWA, $Batalion, $Beratbadan, $Tinggibadan);
 
 if (mysqli_stmt_execute($stmt)) { #jika berhasil, kosongkan old value, beri pesan sukses
   unset($_SESSION['old']);
@@ -92,6 +115,7 @@ if (mysqli_stmt_execute($stmt)) { #jika berhasil, kosongkan old value, beri pesa
     'Nomor'  => $Nomor,
     'Nama' => $Nama,
     'Jabatan' => $Jabatan,
+    'Tanggaljadi' => $Tanggaljadi,
     'Kemampuan' => $Kemampuan,
     'Gaji' => $Gaji,
     'NomorWA' => $NomorWA,
